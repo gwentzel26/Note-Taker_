e2be440ecc7499b735000a6bae0dfd0a8b79e197
Gavin Wentzel <gavinwentzel@gmail.com>
2022-03-11 17:11:15 -0700

    working notes links

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/notes', (req, res) => {
 const newNote = {
       title,
       text,
-      note_id: uuid(),
+      id: uuid(),
     };
 
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
